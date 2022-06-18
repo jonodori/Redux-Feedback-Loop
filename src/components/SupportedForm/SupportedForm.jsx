c86da1615f3c27e3923cdc5982a7a00354d8e1f5
Jonathan Palalay <ring5pun@hotmail.com>
2022-06-18 16:47:09 -0500

    update

---
+++ src/components/SupportedForm/SupportedForm.jsx
@@ -0,0 +1,55 @@
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+
+const UnderstandingForm = () => {
+    //let's you go to the next page 
+    const history = useHistory();
+
+    // in order to use dispatch    
+    const dispatch = useDispatch();
+
+    //state is empty value 
+    const [supported, setSupported] = useState(1);
+
+    const onAddSupported = (evt) => {
+        
+        evt.preventDefault()
+        dispatch({
+            type: 'ADD_SUPPORTED',
+            payload: {
+                supported,
+            }
+        })
+
+        history.push('/comments')
+    }
+
+
+    return (
+        <>
+        <h2>How well are you understanding the content?</h2>
+        <form onSubmit={onAddSupported}>
+            <div>
+                <label>
+                    <input
+                        type="range"
+                        min="1"
+                        max="10"
+                        value={supported}
+                        onChange={evt => setSupported(Number(evt.target.value))}
+                        />
+                </label>
+                    {supported}
+            </div>
+            <div>
+                <button type ="submit">Next</button>
+            </div>
+                
+        </form>
+        </>
+    )
+}
+
+export default SupportedForm;
