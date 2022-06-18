fa4cf1234e78ea4bdf3f64536b01d094733cad8e
Jonathan Palalay <ring5pun@hotmail.com>
2022-06-18 12:49:31 -0500

    finished setting up my server and my GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+
 
 function App() {
+  const dispatch = useDispatch();
+
+useEffect(() => {
+  getFeedback();
+}, []);
+
+const getFeedback = () => {
+  //we go to pizza router 
+  axios({
+    method: 'GET',
+    url: '/api/feedback'
+  }).then(response => {
+    dispatch({
+      type: 'SET_FEEDBACK_LIST',
+      payload: response.data,
+    });
+  }).catch((err) => {
+    console.error('GET /api/pizza failed', err)
+  })
+}
+
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+      </div>
+    </Router>
   );
 }
 
